f98966f0f5940c0102e08e36b7ce77555c4e0711
Austin Hale <93340060+aus36@users.noreply.github.com>
2023-08-05 00:57:36 -0500

    Updates

---
+++ precache-manifest.5333971850534a63f53348b52ffa79e2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad810222f98ef96f117e37debff65124",
+    "url": "/did-wallet-client/index.html"
+  },
+  {
+    "revision": "8634d8cbedf59225510e",
+    "url": "/did-wallet-client/static/css/main.12da2675.chunk.css"
+  },
+  {
+    "revision": "8ed394ef4878bf72fa32",
+    "url": "/did-wallet-client/static/js/2.18c1e9a1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/did-wallet-client/static/js/2.18c1e9a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7823827acf9ac73dbc93",
+    "url": "/did-wallet-client/static/js/3.390d3fd0.chunk.js"
+  },
+  {
+    "revision": "c15eddeb09b46511cf0731116c7067b0",
+    "url": "/did-wallet-client/static/js/3.390d3fd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8634d8cbedf59225510e",
+    "url": "/did-wallet-client/static/js/main.be0f1736.chunk.js"
+  },
+  {
+    "revision": "f8ba8c1da194546bf666",
+    "url": "/did-wallet-client/static/js/runtime-main.4a67c119.js"
+  },
+  {
+    "revision": "b44928671be9d7c53c28540626d82975",
+    "url": "/did-wallet-client/static/media/did.b4492867.mp3"
+  },
+  {
+    "revision": "52dbaa8d50b7953e026dce88e5761e46",
+    "url": "/did-wallet-client/static/media/maoi.52dbaa8d.jpeg"
+  }
+]);
