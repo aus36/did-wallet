108e51bce29d6fa27080053556f692db606571c4
Austin Hale <93340060+aus36@users.noreply.github.com>
2023-08-04 20:58:43 -0500

    Added files from test repository

---
+++ README.md
@@ -0,0 +1,19 @@
+# DID Genrator Web Application
+
+## Description
+This web application gives users the ability to input their information about themselves or the organization they represent, and the website will use this information to return a generated DID document back to them in compliant did:web specification. The application will also generate a basic sigchain verifiable presentation for the user, and allows verification between a hosted did document and verifiable presentation.
+
+### Technologies
+- React
+- Node.js
+- Express
+
+### Installation
+1. Clone the repository
+2. Navigate to the directory using ```cd did-generator```
+3. This project uses node version 16, so ensure that you have nvm installed and run ```nvm use 16```
+4. In a terminal, navigate to ```src/client``` and run ```npm install```
+5. In a terminal, navigate to ```src/server``` and run ```npm install```
+6. Navigate back to the project root and run ```npm run dev``` which will run both the server and client 
+7. Navigate to localhost:3000 in your browser to view the application, if the previous command doesn't automatically do it
+8. The results will be displayed in the console of the browser after clicking "Submit"
---
+++ src/client/src/App.js
@@ -0,0 +1,281 @@
+import maoi from './maoi.jpeg';
+import sound from './did.mp3';
+import './App.css';
+import React, { useState } from 'react';
+import useSound from 'use-sound';
+import { id } from 'ethers/lib/utils';
+
+const KEY_JSON_PATH = "testData.json";
+
+
+
+// inputs: url, subject type, subject name
+// subject type: person, organization, etc
+// subject name: if person then first/last, if org then org name
+
+const didMethod = "did:web:"
+//DUE TO: replace with user input
+const didUrl = "srujn.github.io:did-root"
+const didController = didMethod + didUrl
+
+
+
+const vmCount = 1
+
+const vmTemplate = {
+  "id": didController, //add key identifier
+  "type": "JsonWebKey2020",
+  "controller": didController,
+  "publicKeyJwk": {
+    "kty": "EC",
+    "crv": "secp256k1",
+    "x": "replace-this",
+    "y": "replace-this"
+  }
+}
+
+const didTemplate = {
+  "@context": [
+    "https://www.w3.org/ns/did/v1",
+    "https://w3id.org/security/suites/jws-2020/v1"
+  ],
+  "id": didController,
+  "verificationMethod": [
+
+  ],
+  "assertionMethod": [
+  ]
+}
+
+// const [users, setUsers] = useState([]);
+
+// useEffect(() => {
+//   fetch("/api/users")
+//     .then(res => res.json())
+//     .then(json => setUsers(json.users));
+//   // Specify how to clean up after this effect:
+//   return () => {};
+// }, []); // empty 2nd arg - only runs once
+
+async function submit(url, isOrg, orgName, firstName, lastName) {
+  // print url, isOrg, orgName, firstName, lastName
+  console.log("URL: " + url + "\n" + "isOrg: "
+    + isOrg + "\n" + "orgName: " + orgName + "\n"
+    + "firstName: " + firstName + "\n" + "lastName: "
+    + lastName + "\n");
+  
+  // generate keypair
+  const keypair = await getKeypair();
+  console.log(keypair);
+  const privateKey = keypair[1].priv;
+  const pubX = keypair[2];
+  const pubY = keypair[3];
+
+
+  // generate DID
+  const subjectType = isOrg ? "organization" : "person";
+  const subjectName = isOrg ? orgName : firstName + " " + lastName;
+  const did = await getDID(url, subjectType, subjectName, pubX, pubY);
+  console.log(JSON.stringify(did, null, 2));
+  
+  // generate VC
+  //const vc = await getVC(privateKey, did, "web_service_binding");
+  //console.log(JSON.stringify(vc, null, 2));
+
+  // generate VP
+  const vp = await getVP(privateKey, did);
+  console.log(JSON.stringify(vp, null, 2));
+
+}
+
+async function getKeypair() {
+  return fetch("/api/keypair", {
+    headers : { 
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+     }
+  })
+    .then(res => res.json())
+    .then(json => {
+      return json.keypair;
+    });
+}
+
+async function getDID(url, subjectType, subjectName, pubX, pubY) {
+  return fetch("/api/did", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      url: url,
+      subjectType: subjectType,
+      subjectName: subjectName,
+      pubX: pubX,
+      pubY: pubY
+    })
+  })
+    .then(res => res.json())
+    .then(json => {
+      return json.did;
+    });
+}
+
+async function getVC(key, did, type) {
+  return fetch("/api/vc", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      key: key,
+      did: did,
+      type: type
+    })
+  })
+    .then(res => res.json())
+    .then(json => {
+      return json.vc;
+    });
+}
+
+async function getVP(key, did) {
+  return fetch("/api/vp", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      key: key,
+      did: did
+    })
+  })
+    .then(res => res.json())
+    .then(json => {
+      return json.vp;
+    });
+}
+
+// Functions for validating the did document and vp pair
+//========================================================
+// 1. Fetch the did document from a hosted location
+async function fetchHostedDID(url) {
+  return fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    console.log(JSON.stringify(data, null, 2)); // Prints the did doc to the console
+    return data;
+  })
+  .catch(error => console.error('An error occurred while fetching the did document from '+url+": ", error));
+}
+
+// 2. Fetch the vp from a hosted location
+async function fetchHostedVP(url) {
+  return fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    console.log(JSON.stringify(data, null, 2)); // Prints the vp to the console
+    return data;
+  })
+  .catch(error => console.error('An error occurred while fetching the vp from '+url+": ", error));
+}
+
+// 3. Validate the did document and vp pair
+async function validateDidVp(vpUrl, didUrl) {
+  let vp = await fetchHostedVP(vpUrl);
+  let did = await fetchHostedDID(didUrl);
+  
+  if(vp["verifiableCredentials"][0]["credentialSubject"]["id"] = did["id"]) {
+    return true
+  }
+  else return false
+}
+//========================================================
+
+
+// App component
+function App() {
+  const [play] = useSound(sound);
+
+  const [url, setUrl] = useState("joe.com");
+
+  const [isOrg, setIsOrg] = useState(false);
+  const [orgName, setOrgName] = useState("");
+  const [firstName, setFirstName] = useState("joe");
+  const [lastName, setLastName] = useState("brandon");
+
+  const [vpUrl, setVpUrl] = useState("https://aus36.github.io/didweb-doc/vp.json"); // Url for hosted vp
+  const [didUrl, setDidUrl] = useState("https://aus36.github.io/didweb-doc/did.json"); // Url for hosted did document
+
+  const [loaded, setLoaded] = useState(false); // flag for when the validate pair function has been called
+
+  const [validateSuccessful, setValidateSuccessful] = useState(false); // flag for the result when the validate pair function has been called
+
+  const setType = () => {
+    if (document.getElementById('type').value === 'organization') {
+      setIsOrg(true);
+    } else {
+      setIsOrg(false);
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {<img src={maoi} onClick={play} className="App-logo" alt="logo" />}
+        <form style={{ textAlign: 'left', width: '50%', margin: 'auto' }}
+          onSubmit={e => {
+            e.preventDefault();
+            submit(url, isOrg, orgName, firstName, lastName);
+          }}>
+          <label>
+            URL:
+            <input type="text" value={url} onChange={e => setUrl(e.target.value)} />
+          </label>
+          <br />
+          <label>
+            Subject Type:
+            <select id='type' onChange={setType}>
+              <option value="person">Person</option>
+              <option value="organization">Organization</option>
+            </select>
+          </label>
+          <br />
+          {isOrg ?
+            <label>
+              Organization Name:
+              <input type="text" onChange={e => setOrgName(e.target.value)} />
+            </label> :
+            <label>
+              First Name:
+              <input id="firstName" value={firstName} type="text" onChange={e => setFirstName(e.target.value)} />
+              <br />
+              Last Name:
+              <input id="lastName" value={lastName} type="text" onChange={e => setLastName(e.target.value)} />
+            </label>
+          }
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
+        <br />
+        <form>
+          <label>
+            DID Document URL:
+            <input id="didDocUrl" value={didUrl} type="text" onChange={e => setDidUrl(e.target.value)} />
+            <br />
+            Verifiable Presentation URL:
+            <input id="vpUrl" value={vpUrl} type="text" onChange={e => setVpUrl(e.target.value)} />
+          </label>
+        </form>
+        <br />
+        <button onClick={ () => {setValidateSuccessful(validateDidVp(vpUrl, didUrl)); setLoaded(true);}}>Validate did document and vp pair</button>
+        <br />
+        {loaded
+        ? <p>Validation result: {validateSuccessful ? "Pair Sucessfully Validated" : "Invalid DID Doc/VP Pair"}</p> 
+        : <p>Nothing to validate yet</p>}
+      </header>
+    </div>
+  );
+}
+
+export default App;
